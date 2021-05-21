d8334b380a4eb0d3e144fd99f12d11ab93705ad2
lery2580 <73690399+lery2580@users.noreply.github.com>
2021-05-22 00:20:24 +0800

    first commit

--- page.js
+++ page.js
@@ -1,23 +1,28 @@
 let pageNumber = 0
 
 
-
 const nextTag = document.querySelector("footer img.next")
 const ouputTag = document.querySelector("h2")
 
-const updateSection = function(){
-
-}
 
 const next = function(){
+
 pageNumber = pageNumber +1
+
+if(pageNumber > 10){
+    pageNumber = 1
+}
+    updateSection()
 }
 
+const updateSection = function(){
+
+    ouputTag.innerHTML = pageNumber
+
+}
 
 nextTag.addEventListener("click", function(){
 
-    
-    pageNumber = pageNumber + 1
-
-    ouputTag.innerHTML = pageNumber
+next()
 })
+
