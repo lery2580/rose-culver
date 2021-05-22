bb4b7c10b6b16ce4041a54e83e8de9a3ebfd2fd5
lery2580 <73690399+lery2580@users.noreply.github.com>
2021-05-22 22:46:41 +0800

    fitst commit

--- page.js
+++ page.js
@@ -1,23 +1,39 @@
 let pageNumber = 0
 
+const pages = [
+
+    "brooklyn design",
+    "kanye west",
+    "october",
+    "download my pdf"
+]
 
 const nextTag = document.querySelector("footer img.next")
 const ouputTag = document.querySelector("h2")
+const previousTag = document.querySelector("footer img.prev")
 
+
+const previous = function(){
+    pageNumber = pageNumber -1
+    if(pageNumber < 0){
+        pageNumber = pages.length -1
+    }
+    updateSection()
+}
 
 const next = function(){
 
 pageNumber = pageNumber +1
 
-if(pageNumber > 10){
-    pageNumber = 1
+if(pageNumber > pages.length -1){
+    pageNumber = 0
 }
     updateSection()
 }
 
 const updateSection = function(){
 
-    ouputTag.innerHTML = pageNumber
+    ouputTag.innerHTML = pages[pageNumber]
 
 }
 
@@ -26,3 +42,7 @@ nextTag.addEventListener("click", function(){
 next()
 })
 
+
+previousTag.addEventListener("click",function(){
+    previous()
+})
